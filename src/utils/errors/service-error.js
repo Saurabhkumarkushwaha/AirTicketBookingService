73c9f5f0e4046e1de03dedf7c746d738ece6b80f
Saurabh Kushwaha <sk2005@gmail.com>
2025-05-24 15:42:36 +0530

    Completed created booking

--- src/utils/errors/service-error.js
+++ src/utils/errors/service-error.js
@@ -2,10 +2,11 @@ const { StatusCodes } = require('http-status-codes');
 
 class ServiceError extends Error {
     constructor(
-        message,
-        explanation,
-        statusCodes = StatusCodes.INTERNAL_SERVER_ERROR
+        message = 'Something Went Wrong',
+        explanation = 'Service layer error',
+        statusCode = StatusCodes.INTERNAL_SERVER_ERROR
     ) {
+        super();
         this.name = 'ServiceError';
         this.message = message;
         this.explanation = explanation;
