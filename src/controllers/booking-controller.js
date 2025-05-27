24058f5f9de7fc53f43f5cf9a20cb5054c9b992d
Saurabh Kushwaha <sk2005@gmail.com>
2025-05-27 15:26:53 +0530

    done

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -14,8 +14,16 @@ class BookingController {
 
     async sendMessageToQueue(req, res) {
         const channel = await createChannel();
-        const data = {message: 'Success'};
-        publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+        const payload = {
+            data: {
+                subject: 'This is a noti from queue',
+                content: 'Some queue will subscribe this',
+                recepientEmail: 'saurabh6205484703@gmail.com',
+                notificationTime: '2025-05-27T02:55:00.0000'
+            },
+            service: 'CREATE_TICKET'
+        };
+        publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(payload));
         return res.status(200).json({
             message: 'Successfully published the event'
         });
